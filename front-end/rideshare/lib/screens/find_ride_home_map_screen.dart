0dffa16306503cd70b64c9253ac63f0a6d8b908d
KiranRaghavendra1248 <kiran.raghu.1248@gmil.com>
2024-04-13 12:37:11 -0700

    Map pans to current location with Marker added to current location

--- front-end/rideshare/lib/screens/find_ride_home_map_screen.dart
+++ front-end/rideshare/lib/screens/find_ride_home_map_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class FindRideHomeMapScreen extends StatefulWidget {
@@ -12,9 +13,10 @@ class FindRideHomeMapScreen extends StatefulWidget {
 
 class _FindRideHomeMapScreenState extends State<FindRideHomeMapScreen> {
 
-  final Completer<GoogleMapController> _controller = Completer();
-  
+  late GoogleMapController _controller;
   static const CameraPosition initialPosition = CameraPosition(target: LatLng(33.684566, -117.826508), zoom:14.0);
+
+  Set<Marker> markers = {};
 
   @override
   Widget build(BuildContext context) {
@@ -28,14 +30,53 @@ class _FindRideHomeMapScreenState extends State<FindRideHomeMapScreen> {
         children: [
             GoogleMap(
             initialCameraPosition: initialPosition,
+            zoomControlsEnabled: false,
+            markers: markers,
             mapType: MapType.normal,
             onMapCreated: (GoogleMapController controller) {
-              _controller.complete(controller);
+              _controller = controller;
+              _determineLocation();
             }
             ),
 
         ],
       )
     );
+  }
+
+  Future<void> _determineLocation() async{
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if(!serviceEnabled){
+      return Future.error('Location services are disabled');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if(permission == LocationPermission.denied){
+      // Request permission
+      permission = await Geolocator.requestPermission();
+      if(permission == LocationPermission.denied){
+        return Future.error('Location permission denied');
+      }
+    }
+    if(permission == LocationPermission.deniedForever){
+      return Future.error('Location permissions are permanently denied');
+    }
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high
+    );
+    _controller.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(target: LatLng(position.latitude,position.longitude), zoom: 14.0)
+        )
+    );
+    markers.clear();
+    markers.add(
+      Marker(markerId: MarkerId('currentLocation'),
+      position: LatLng(position.latitude,position.longitude))
+    );
+    setState(() {});
   }
 }
