709e4e822404a3faf6dbbed2929e89798a5c84ef
KiranRaghavendra1248 <kiran.raghu.1248@gmil.com>
2024-04-13 22:34:50 -0700

    Added form fields for time with time picker enabled

--- front-end/rideshare/lib/screens/find_ride_home_map_screen.dart
+++ front-end/rideshare/lib/screens/find_ride_home_map_screen.dart
@@ -11,50 +11,11 @@ class FindRideHomeMapScreen extends StatefulWidget {
   State<FindRideHomeMapScreen> createState() => _FindRideHomeMapScreenState();
 }
 
-class InputForm extends StatelessWidget{
-  final _startsearchFieldController = TextEditingController();
-  final _endsearchFieldController = TextEditingController();
-  @override
-  Widget build(BuildContext context) {
-    return Positioned(
-        top: 2,
-        left: 0,
-        right: 0,
-        child: Padding(
-        padding: EdgeInsets.all(10),
-        child: Container(
-          color: Colors.lightBlue[50],
-          padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
-          child: Column(
-            children: [
-              TextField(
-                controller: _startsearchFieldController,
-                autofocus: false,
-                decoration: InputDecoration(
-                  hintText: 'Start Location',
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: InputBorder.none,
+class InputForm extends StatefulWidget{
+  const InputForm({super.key});
 
-                ),
-              ),
-              SizedBox(height: 10),
-              TextField(
-                controller: _endsearchFieldController,
-                autofocus: false,
-                decoration: InputDecoration(
-                  hintText: 'End Location',
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: InputBorder.none,
-                ),
-              ),
-            ],
-          ),
-        ),
-        )
-        );
-      }
+  @override
+  State<InputForm> createState() => _InputForm();
 }
 
 class _FindRideHomeMapScreenState extends State<FindRideHomeMapScreen> {
@@ -129,5 +90,136 @@ class _FindRideHomeMapScreenState extends State<FindRideHomeMapScreen> {
             position: LatLng(position.latitude, position.longitude))
     );
     setState(() {});
+  }
+}
+
+class _InputForm extends State<InputForm>{
+  final _startTimeController = TextEditingController();
+  final _endTimeController = TextEditingController();
+  final _seatsController = TextEditingController(text: '1');
+  final _startsearchFieldController = TextEditingController();
+  final _endsearchFieldController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+        top: 2,
+        left: 0,
+        right: 0,
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Container(
+            color: Colors.lightBlue[50],
+            padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+            child: Column(
+              children: [
+                TextField(
+                  controller: _startsearchFieldController,
+                  autofocus: false,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 15.0),
+                    hintText: 'Start Location',
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0), // Adjust the radius as needed
+                      borderSide: BorderSide.none, // Make the border invisible
+                    ),
+                    prefixIcon: Icon(
+                        Icons.radio_button_checked,
+                        color: Colors.deepPurple[100]
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  controller: _endsearchFieldController,
+                  autofocus: false,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 15.0),
+                    hintText: 'Destination Location',
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0), // Adjust the radius as needed
+                      borderSide: BorderSide.none, // Make the border invisible
+                    ),
+                    prefixIcon: Icon(
+                      Icons.radio_button_checked,
+                      color: Colors.deepOrange[100],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: _startTimeController,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 15.0),
+                          labelText: 'Start time',
+                          floatingLabelBehavior: FloatingLabelBehavior.always,
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0), // Adjust the radius as needed
+                            borderSide: BorderSide.none, // Make the border invisible
+                          ),
+                          prefixIcon: Icon(
+                            Icons.schedule,
+                            color: Colors.deepPurple[100],
+                          ),
+                        ),
+                        readOnly: true, // Make the field read-only
+                        onTap: () {
+                          // Open a date and time picker when the field is tapped
+                          _selectTime(context, _startTimeController);
+                        },
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: TextFormField(
+                        controller: _endTimeController,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 15.0),
+                          labelText: 'End time',
+                          floatingLabelBehavior: FloatingLabelBehavior.always,
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0), // Adjust the radius as needed
+                            borderSide: BorderSide.none, // Make the border invisible
+                          ),
+                          prefixIcon: Icon(
+                          Icons.schedule,
+                          color: Colors.deepOrange[100],
+                        ),
+                        ),
+                        readOnly: true, // Make the field read-only
+                        onTap: () {
+                          // Open a date and time picker when the field is tapped
+                          _selectTime(context, _endTimeController);
+                        },
+                      )
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        )
+    );
+  }
+
+  Future<void> _selectTime(BuildContext context, TextEditingController controller) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    );
+    if (picked != null) {
+      controller.text = picked.format(context);
+    }
   }
 }
