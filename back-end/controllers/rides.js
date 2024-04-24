9520da0b7079e7bded3b9991d6f60e6ee04e43fe
KiranRaghavendra1248 <kiran.raghu.1248@gmil.com>
2024-04-24 00:01:18 -0700

    Renamed some files and added common network util for API calls to backend

--- back-end/controllers/rides.js
+++ back-end/controllers/rides.js
@@ -1,45 +1,49 @@
-
 // created placeholders
+// if you add new api handling functions, dont forget to add it to exports at the end :)
 
-const signUpUser = async (req,res)=>{
+const signUpUser = async (req, res) => {
 
-}
+};
 
-const loginUser = async (req,res)=>{
+const loginUser = async (req, res) => {
 
-}
+};
 
-const getUserDetails = async (req,res)=>{
+const getUserDetails = async (req, res) => {
 
-}
+};
 
-const modifyUserDetails = async (req,res)=>{
+const modifyUserDetails = async (req, res) => {
 
-}
+};
 
-const submitRide = async (req,res)=>{
+const submitRide = async (req, res) => {
 
-}
+};
 
-const findRides = async (req,res)=>{
+const findRides = async (req, res) => {
+    const response = {
+        message : "Hello World!!"
+    };
+    console.log(req);
+    res.status(200).json(response);
+};
 
-}
+const getRideDetails = async (req, res) => {
 
-const getRideDetails = async (req,res)=>{
+};
 
-}
+const confirmRide = async (req, res) => {
 
-const confirmRide = async (req,res)=>{
-
-}
+};
 
 module.exports = {
-    signUpUser,
-    loginUser,
-    getUserDetails,
-    modifyUserDetails,
-    submitRide,
-    findRides,
-    getRideDetails,
-    confirmRide,
-  }
+  signUpUser,
+  loginUser,
+  getUserDetails,
+  modifyUserDetails,
+  submitRide,
+  findRides,
+  getRideDetails,
+  confirmRide,
+};
