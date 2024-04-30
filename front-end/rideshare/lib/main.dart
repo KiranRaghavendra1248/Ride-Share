21810a587002a90a612f02740f25f209621dc247
suhas-bn-1412 <suhasbn1412@gmail.com>
2024-04-30 14:49:31 -0700

    Integrated firebase messaging service to the project

--- front-end/rideshare/lib/main.dart
+++ front-end/rideshare/lib/main.dart
@@ -1,10 +1,27 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'package:rideshare/screens/welcome_screen.dart';
 import 'package:rideshare/theme/theme.dart';
 
+
+Future<void> getFCMToken() async {
+  String? token = await FirebaseMessaging.instance.getToken();
+  print("FirebaseMessaging token: $token");
+}
+
 Future<void> main() async {
   await dotenv.load();
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await getFCMToken();
+  // FirebaseMessaging.onBackgroundMessage(_backgroundHandler);
+
   runApp(const MyApp());
 }
 
